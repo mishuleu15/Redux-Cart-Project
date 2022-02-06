0288a464ad2e9d25bf505a45901f763434b98323
Catalin Mihai <mishu_leu15@yahoo.com>
2022-02-06 17:35:34 +0200

    redux cart project

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// components
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+
+// redux stuff
+import { createStore } from 'redux';
+import reducer from './reducer';
+
+import { Provider } from 'react-redux';
+
+// store
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+function App() {
+  // cart setup
+
+  return (
+    <main>
+      <Provider store={store}>
+        <Navbar />
+        <CartContainer />
+      </Provider>
+    </main>
+  );
+}
+
+export default App;
